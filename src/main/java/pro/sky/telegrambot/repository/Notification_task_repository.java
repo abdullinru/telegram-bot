5d148b6e4c56b7db1f4bba562c804ac7a35396e1
abdullinru <abdullin17021989@gmail.com>
2022-11-04 23:05:34 +0600

    мелкие правки

--- src/main/java/pro/sky/telegrambot/repository/Notification_task_repository.java
+++ src/main/java/pro/sky/telegrambot/repository/Notification_task_repository.java
@@ -3,5 +3,5 @@ package pro.sky.telegrambot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.sky.telegrambot.model.Notification_task;
 
-public interface notification_taskRepository extends JpaRepository<Notification_task, Long> {
+public interface Notification_task_repository extends JpaRepository<Notification_task, Long> {
 }
